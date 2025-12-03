3aa2ede3722b1ad09df7b7727c96dd4427a0c6a2
Guvanch M <158216778+cspinnacle@users.noreply.github.com>
2025-12-03 17:25:05 +0000

    Solution Test

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 # This is your first Python program
 
 # The print() function displays text on the screen
-print("Hello, World!")
+print("Hello Pinnacle")
+
+name = input("What is you name: ")
+print("Hello " + name)
+
+number = int(input("Enter a number"))
+print(number * number)
+
+
 
 # Try printing your own message below:
 # print("Your message here")
